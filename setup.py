2f467db3ecf3993cd73f19ecb35f64e43a700f09
Willi Rath <willirath@users.noreply.github.com>
2021-02-06 13:06:52 +0100

    Release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="intake_zenodo_fetcher",
-    version="2021.02.05.1",
+    version="2021.02.06.1",
     description="Fetch data from Zenodo based on Intake catalog entries",
     url="https://github.com/ESM-VFC/intake_zenodo_fetcher",
     author="ESM-VFC developers",
